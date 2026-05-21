72e5a24247458e40da8a2ac89644c555a0cac594
Keshav <ketiwari@ucsd.edu>
2026-05-21 03:09:09 -0700

    highlight selection and button mapping

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-/*document.addEventListener("selectionchange", () => { 
-    console.log(document.getSelection());
-})*/
+document.addEventListener("mouseup", () => {
+    const text = document.getSelection().toString();
+    const checkBtn = document.getElementById("hBtn");
+    if (checkBtn){
+        checkBtn.remove();
+    }
 
-document.onselectionchange = () => {
-    console.log(document.getSelection());
-  };
-  
-console.log("extension check!")
+    if (text){
+        const coord = document.getSelection().getRangeAt(0).getBoundingClientRect();
+        
+        const newBtn = document.createElement("button");
+        newBtn.textContent = "Click Me";
+        newBtn.type = "button";    
+        newBtn.id = "hBtn";
+
+        document.body.appendChild(newBtn);
+        newBtn.style.position = "fixed";
+        newBtn.style.left = `${coord.x + coord.width}px`;
+        newBtn.style.top = `${coord.y - coord.height - 5}px`;
+    }
+})
