ac74cc803ea39154e9ac4a81cbf28bb82280b939
Keshav <ketiwari@ucsd.edu>
2026-06-01 21:21:27 -0700

    WIP rehighlighting, context in db

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ document.addEventListener("mouseup", () => {
 
     if (!text){return}
 
-    const coord = document.getSelection().getRangeAt(0).getBoundingClientRect();
+    const range = document.getSelection().getRangeAt(0)
+    const coord = range.getBoundingClientRect();
+    const nodeText = range.startContainer.textContent
+    const prefix = nodeText.slice(Math.max(0, range.startOffset - 50), range.startOffset)
+    const suffix = nodeText.slice(range.endOffset, range.endOffset + 50)
+
     
     const newBtn = document.createElement("button");
     newBtn.textContent = "Click Me";
@@ -27,7 +32,7 @@ document.addEventListener("mouseup", () => {
 
     newBtn.addEventListener("click", async (event) => {
         console.log('clicked')
-        const { error } = await hDB.from('highlights').insert({ content: text, source_url: window.location.href})
+        const { error } = await hDB.from('highlights').insert({ content: text, source_url: window.location.href, prefix: prefix, suffix: suffix})
         if (error) console.error(error)
     });
 
@@ -36,3 +41,48 @@ document.addEventListener("mouseup", () => {
     });
     
 })
+
+async function rehighlight(){
+
+    const { data, error } = await hDB
+    .from('highlights')
+    .select('content, prefix, suffix')
+    .eq('source_url', window.location.href)
+
+    if (data.length == 0){
+        return;
+    } else {
+
+        data.forEach(highlight => {
+
+            if (document.body.includes((highlight.prefix + highlight.content + highlight.suffix))){
+                const range = document.createRange();
+                range.setStart()
+
+                const treeWalker = document.createTreeWalker(
+                    document.querySelector("body"),
+                    NodeFilter.SHOW_TEXT,
+                );
+
+                while (treeWalker.nextNode()) {
+                    const node = treeWalker.currentNode;
+                    node.data = node.data.toUpperCase();
+                }
+
+                const mark = document.createElement("mark");
+                range.surroundContents(mark);
+            }
+        })
+
+    }
+
+
+
+
+
+}
+
+
+    
+
+rehighlight()
