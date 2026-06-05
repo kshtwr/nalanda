ac38ba7e6a7aa655c5ebf0382ebed94154d7090c
Keshav <ketiwari@ucsd.edu>
2026-06-04 21:57:16 -0700

    updated auth working now

--- nalanda-dashboard/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  console.log('middleware running')
-  return NextResponse.redirect(new URL('/login', request.url))
-}
-
