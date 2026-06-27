e55e516edf5e526e24e843465d84a18b82f13f0c
Keshav <ketiwari@ucsd.edu>
2026-06-26 17:20:05 -0700

    sign-in bugs, feedback on link hover

--- popup.js
+++ popup.js
@@ -1,29 +1,24 @@
-document.getElementById('signInBtn').addEventListener('click', () => {
-    chrome.tabs.create({ url: 'https://nalanda-highlights.vercel.app' })
-})
+const SUPABASE_URL = 'https://mvuafuijosnsgtglomfx.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_YOoDS5GlFfbfJgmRGUIEqQ_bFXpi0o_'
 
-async function rehighlight(){
+const { createClient } = supabase;
+const hDB = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function popupDisplay(){
     const result = await chrome.storage.local.get('session')
     if (result.session) await hDB.auth.setSession(result.session)
-    const pageUrl = new URL(window.location.href)
-    pageUrl.searchParams.delete('nalanda_id')
-    const cleanUrl = pageUrl.toString()
 
-
-    const { data, error } = await hDB
-    .from('highlights')
-    .select('id,content')
-    .eq('source_url', cleanUrl)
+    const { data, error } = await hDB.from('highlights').select('id, content, source_url').order('created_at', { ascending: false }).limit(5)
 
     if (error || !data || data.length == 0) return;
 
     data.forEach(highlight => {
-        markText(highlight.id, highlight.content);
+
     })
-    const id = new URLSearchParams(window.location.search).get('nalanda_id')
-    const target = document.querySelector(`.highlight-${id}`)
-    target?.scrollIntoView({behavior:'smooth', block:'center'})
-    target.style.transition = 'background-color 1s'
-    target.style.backgroundColor = 'orange'
-    setTimeout(() => { target.style.backgroundColor = '' }, 1000)
 }    
+
+document.getElementById('signInBtn').addEventListener('click', () => {
+    chrome.tabs.create({ url: 'https://nalanda-highlights.vercel.app' })
+})
+
+popupDisplay();
