111db2d79ed91d7100f976391b95ddb9467dc73b
Keshav <ketiwari@ucsd.edu>
2026-07-01 23:55:29 -0700

    popup updates

--- popup.js
+++ popup.js
@@ -6,19 +6,41 @@ const hDB = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function popupDisplay(){
     const result = await chrome.storage.local.get('session')
-    if (result.session) await hDB.auth.setSession(result.session)
+    if (!result.session) {
+        document.getElementById('signInBtn').style.display = 'block'
+        return
+    }
+    await hDB.auth.setSession(result.session)
+    
 
-    const { data, error } = await hDB.from('highlights').select('id, content, source_url').order('created_at', { ascending: false }).limit(5)
-
-    if (error || !data || data.length == 0) return;
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    const pageUrl = new URL(tab.url)
+    pageUrl.searchParams.delete('nalanda_id')
+    const cleanUrl = pageUrl.toString()
+        
+    const { data, error } = await hDB.from('highlights').select('id, content, source_url').order('created_at', { ascending: false }).eq('source_url', cleanUrl)
+    console.log(data, error)
+    if (error || !data || data.length == 0){
+        const card = document.createElement('p')
+        card.innerText = "No highlights found."
+        document.body.appendChild(card)
+        return
+    };
 
     data.forEach(highlight => {
-
+        const card = document.createElement('div')
+        card.className = 'card'
+        card.textContent = highlight.content.length > 100 
+            ? highlight.content.slice(0, 100) + '...' 
+            : highlight.content
+        document.body.appendChild(card)
     })
+    
 }    
 
 document.getElementById('signInBtn').addEventListener('click', () => {
-    chrome.tabs.create({ url: 'https://nalanda-highlights.vercel.app' })
+    //chrome.tabs.create({ url: 'https://nalanda-highlights.vercel.app' })
+    chrome.tabs.create({ url: 'http://localhost:3000' })
 })
 
 popupDisplay();
